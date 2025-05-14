c5a1fe90b7550a9df4450ab0706f0cea87e9dc17
Devin Xie <devinxie418@gmail.com>
2025-05-14 14:01:46 -0400

    implemented main functionalities

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react"
+import { db } from '../firebase.js';
+import { collection, addDoc } from 'firebase/firestore';
 
 
 export function NoteForm() {
+    const [note, setNote] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if(!note.trim()) {
+            return;
+        }
+
+        try { 
+            await addDoc(collection(db, "notes"), {
+                content: note
+            })
+            console.log("Successfully added note: ", note);
+            setNote("");
+        }
+        catch (error) {
+            console.error("Error adding note: ", error);
+        }
+    }; 
+
     return (
-        <>
-            
-        </>
+        <form onSubmit={e => handleSubmit(e)}>
+            <input
+            value={note}
+            onChange={(e) => {setNote(e.target.value)}}
+            placeholder="Write a note..."
+            />   
+            <button type="submit">Add Note</button>
+        </form>
     )
 }
---
+++ src/components/NotesList.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc
+} from 'firebase/firestore';
+
+export function NotesList() {
+    const [notes, setNotes] = useState([]);
+
+    async function fetchNotes() {
+        try {
+            const snapshot = await getDocs(collection(db, "notes"));
+            setNotes(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})));
+        }
+        catch (error) {
+            console.error("Error fetching notes: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchNotes();
+    }, [notes]);
+
+    async function handleUpdate(originalContent, id) {
+        const newContent = prompt("Edit content:", originalContent);
+        try {
+            await updateDoc(doc(db, "notes", id), {content: newContent});
+            fetchNotes();
+        }
+        catch (error) {
+            console.error("Error updating note: ", error);
+        }
+    }
+
+    async function handleDelete(id) {
+        try {
+            await deleteDoc(doc(db, "notes", id));
+            fetchNotes();
+        }
+        catch (error) {
+            console.error("Error deleting note: ", error);
+        }
+    }
+
+    return (
+        <div className='notes-view'>
+            {notes.map(doc => (
+                <div key={doc.id}>
+                    <p>{doc.content}</p>
+                    <button onClick={() => handleUpdate(doc.data, doc.id)}>Edit</button>
+                    <button onClick={() => handleDelete(doc.id)}>Delete</button>
+                </div>
+            ))
+            }
+        </div>
+    )
+}
